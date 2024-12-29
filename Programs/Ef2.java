a26b7377b48c017ef3932a14a0463286307594e4
Devasena K <143887926+devasena08@users.noreply.github.com>
2024-12-29 21:59:29 +0530

    Update Ef2.java

--- Programs/Ef2.java
+++ Programs/Ef2.java
@@ -5,11 +5,11 @@ public class Ef2 {
         System.out.println(method(n));
     }
     public static int method(int n){
-        if(n < 9 ){
+        if(n < 9 ){ // n =1,2..8 = 1 digit dhn,so..retrun n
             return n;
-        }else if(n % 9 == 0){
+        }else if(n % 9 == 0){ // n=9,18,27..90 return 9
             return  9;
         }
-        return n%9;
+        return n%9; // remainder 129%9 = 3
     }
 }
