b7996f616cf86d46f267c21bf2f07876640b4ec9
Devasena K <143887926+devasena08@users.noreply.github.com>
2024-12-29 22:52:25 +0530

    Update Hr3.java

--- Programs/Hr3.java
+++ Programs/Hr3.java
@@ -1,13 +1,15 @@
 public class Hr3 {
     public static void main(String[] args){
-        //palindrome
-        String s = "Medam";
+        //palindrome-->digits also allowed e:- 123deaade321
+        String s = "Madam";
         String str = s.toLowerCase();
         System.out.println(isPalindrome(str));
     }
+    //use two pointer approach
     public static boolean isPalindrome(String str){
         int st = 0;
         int end = str.length() - 1;
+        // < less than mtm dhn...not <= 
         while (st < end) {
             
             if (!Character.isLetter(str.charAt(st))) {
