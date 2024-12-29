1e8e66d891e4d2bd6cb9e3c9f88c47210e8b885d
Devasena K <143887926+devasena08@users.noreply.github.com>
2024-12-29 22:35:48 +0530

    Update Hr1.java

--- Programs/Hr1.java
+++ Programs/Hr1.java
@@ -5,12 +5,15 @@ public class Hr1{
         String s = "aabbcc"; //abbcc=false
         HashMap<Character,Integer> map = new HashMap<>();
         HashSet<Integer> set = new HashSet<>();
+        //map to store frequency count
         for(char ch : s.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
+        //inserting into set,...which does not hold duplicate values
         for (int value : map.values()) {
             set.add(value);
         }
+        //if size of the set == 1..then it is valid string
         if (set.size() == 1) {
             System.out.println("True..");
         } else {
