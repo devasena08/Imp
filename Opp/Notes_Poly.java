3dc794d7ba0e2b94236081d0faed534596f88d6d
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-04 22:00:15 +0530

    Update Notes_Poly.java

--- Opp/Notes_Poly.java
+++ Opp/Notes_Poly.java
@@ -9,7 +9,7 @@ class Notes_Poly{
      * overriding means..same method name having same  arguments and same datatype 
      * in this, diff objects which method is to called is decided by the object at the runtime
      * bcz, it decides which method is first to execute in run time itself..and that is dynamic..
-     * Method Overloading is Complie time poly / Static
+     * Method Overriding is Runtime time poly / Dynamic
      * child classs give own defintion than parent class is method overriding
      */
 }
