f06a1605705b758496094484adff9f81382a13b3
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-07 18:00:33 +0530

    Update Fern12.java

--- Comp/Fern12.java
+++ Comp/Fern12.java
@@ -1,13 +1,14 @@
 public class Fern12 {
     public static void main(String[] args) {
+        //first non repeating letter
         String str = "swiss";
-        String s = str.toLowerCase();
-        for (int i = 0; i < str.length(); i++) {
-            int count = 0;
-            for (int j = 0; j < str.length(); j++) {
+        String s = str.toLowerCase();//swiss
+        for (int i = 0; i < str.length(); i++) { //s //w--->it goes like this
+            int count = 0;//used to reset the count to zero
+            for (int j = 0; j < str.length(); j++) {//s,w,i,s,
 
-                if (s.charAt(i) == s.charAt(j) && i!=j) {
-                    count++;
+                if (s.charAt(i) == s.charAt(j) && i!=j) { // i!=jfails,fails,fails,satis
+                    count++; //s,w,i,s(1),s(2)
                 }
                 
             }
