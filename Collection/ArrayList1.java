c261ec72e1442442b1b95cced884df87d87656fa
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-07 16:58:45 +0530

    Update ArrayList1.java

--- Collection/ArrayList1.java
+++ Collection/ArrayList1.java
@@ -3,20 +3,27 @@ import java.util.ArrayList;
 public class ArrayList1 {
     public static void main(String[] args) {
         ArrayList<Integer>  al1 = new ArrayList<>();
-        al1.add(10);
-        al1.add(0,5);
-        System.out.println(al1);
-        al1.addFirst(1);
-        System.out.println(al1);
-        al1.addLast(100);
-        System.out.println(al1);
-        al1.remove(1);
-        System.out.println(al1);
-        al1.removeFirst();
-        System.out.println(al1);
-        al1.removeLast();
-        System.out.println(al1);
+        al1.add(10);  //10
+        al1.add(0,5); //5,10
+        System.out.println(al1);//5,10
+        al1.addFirst(1);//1,5,10 
+        System.out.println(al1);//1,5,10 
+        al1.addLast(100);//1,5,10,100
+        System.out.println(al1);//1,5,10,100
+        al1.remove(1);//1,10,100
+        System.out.println(al1);//1,10,100
+        al1.removeFirst();//10,100
+        System.out.println(al1);//10,100
+        al1.removeLast();//10
+        System.out.println(al1);//10
         
-        /* add, remove, addfirst,removefirst, addlast, removelast */
+        /* add, remove, addfirst,removefirst, addlast, removelast
+[5, 10]
+[1, 5, 10]
+[1, 5, 10, 100]
+[1, 10, 100]
+[10, 100]
+[10]
+*/
     }
 }
