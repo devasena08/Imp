51ff34907c15ee1f4e4cea81d53e9ffa744c64d3
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-07 17:17:12 +0530

    Update LinkedListDemo.java

--- Collection/LinkedListDemo.java
+++ Collection/LinkedListDemo.java
@@ -8,18 +8,25 @@ public class LinkedListDemo {
         ll.add(3);
         ll.add(4);
         ll.add(5);
-        System.out.println(ll);
+        System.out.println(ll);//1,2,3,4,5
         // remove is equal to poll
-        System.out.println(ll.poll());// will remove the first index value
-        System.out.println(ll);
-        //offer
-        ll.offer(500);
-        System.out.println(ll);
+        System.out.println(ll.poll());// will remove the first index value---> 1
+        System.out.println(ll);//2,3,4,5
+        //offermenas adding
+        ll.offer(500);//2,3,4,5,500
+        System.out.println(ll); //2,3,4,5,500
         // clone
         //LinkedList<Integer> l2 = (LinkedList)ll.clone();
         //System.out.println(l2);
         // to clear all
-        ll.clear();
+        ll.clear();//l1 is got cleared
         System.out.println(ll);
+        /*
+[1, 2, 3, 4, 5]
+1
+[2, 3, 4, 5]
+[2, 3, 4, 5, 500]
+[]
+*/
     }
 }
