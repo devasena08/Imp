1eb8e5901fc8911a2921fc47f5ae04efc095faba
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-07 18:01:19 +0530

    Update Fern14.java

--- Comp/Fern14.java
+++ Comp/Fern14.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Fern14 {
     public static void main(String[] args) {
+        //remove duplicates
         int[] arr ={2,4,6,8,1,2,6};
         HashSet<Integer> set = new HashSet<>();
         for(int num : arr){
