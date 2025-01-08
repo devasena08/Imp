481f72063cfaf675a182fe565f9964bc13af22d1
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-08 22:20:15 +0530

    Update Fern2.java

--- Comp/Fern2.java
+++ Comp/Fern2.java
@@ -1,5 +1,6 @@
 public class Fern2 {
     public static void main(String[] args) {
+        //linear search
         int[] arr ={1,2,3,4,5,6};
         int target =4;
         for(int i=0; i<arr.length; i++){
