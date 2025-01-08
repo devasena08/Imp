b865c56c0255f18a13c3ad537de81be32de37d4f
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-08 22:41:33 +0530

    Update Fern5.java

--- Comp/Fern5.java
+++ Comp/Fern5.java
@@ -1,5 +1,6 @@
 public class Fern5 {
     public static void main(String[] args) {
+        //sum of elements in 2d array
         int[][] arr = {{1,2,3},
                        {4,5,6}};
                        
@@ -13,4 +14,14 @@ public class Fern5 {
         
     }
 }
- 
+/*
+Bc...it 3*2
+For i = 0 (first row):
+j = 0 → Process arr[0][0] (value 1).
+j = 1 → Process arr[0][1] (value 2).
+j = 2 → Process arr[0][2] (value 3).
+For i = 1 (second row):
+j = 0 → Process arr[1][0] (value 4).
+j = 1 → Process arr[1][1] (value 5).
+j = 2 → Process arr[1][2] (value 6).
+*/
