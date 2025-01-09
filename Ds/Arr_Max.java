e7953c62da83f59e123aae8f6e8bf3d2eb2f85c4
Devasena K <143887926+devasena08@users.noreply.github.com>
2025-01-09 15:37:38 +0530

    Update Arr_Max.java

--- Ds/Arr_Max.java
+++ Ds/Arr_Max.java
@@ -1,5 +1,6 @@
 public class Arr_Max {
     public static void main(String[] args) {
+        //max element in the array
         int[] arr ={12,34,2,33,1};
         int max =0;
         for(int num : arr){
